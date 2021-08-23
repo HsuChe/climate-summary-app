400d188d0d3c2c5a06c28cb1b45824e8e9593705
Daniel Chen <hsueh.l.chen@outlook.com>
2021-08-23 19:03:12 -0400

    update app.py

--- app.py
+++ app.py
@@ -17,13 +17,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return('Hello World')
+    return(
+         f"Available Routes:<br/>"
+        f"Precipitation Information: /api/v1.0/precipitation<br/>"
+        f"List of Stations in the Dataset: /api/v1.0/stations<br/>"
+        f"Temperature - Year: /api/v1.0/tobs<br/>"
+        f"Temperature stat from Certain Starting Date(yyyy-mm-dd): /api/v1.0/yyyy-mm-dd<br/>"
+        f"Temperature stat from Range of Dates(yyyy-mm-dd): /api/v1.0/yyyy-mm-dd/yyyy-mm-dd"
+        )
 
-@app.route("/<query>")
-def get_query(query):
+@app.route("/api/v1.0/<query_date>")
+def get_query(query_date):
     session = Session(conn)
+    query_result = session.query(
+        func.min(Measurement.tobs),
+        func.avg(Measurement.tobs),
+        func.max(Measurement.tobs)
+    ).filter(Measurement.date >= query).all()
     session.close()
-    return(query)
+    
+    for min, avg, max in query_result:
+        print(min,avg,max)
+    
+    return jsonify(query_result)
 
 
 
